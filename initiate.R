8bf019dded36533e242b29e42fef2981737f779a
scholarcraft <58997358+joe-chelladurai@users.noreply.github.com>
2022-11-21 09:22:57 -0800

    Update

--- initiate.R
+++ initiate.R
@@ -6,3 +6,36 @@ suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(janitor))
 suppressPackageStartupMessages(library(gt))
 suppressPackageStartupMessages(library(gtExtras))
+
+
+
+
+
+all <- fotmob_get_league_matches(league_id = 77)
+
+
+matches <- all %>% select(page_url, id) %>% 
+  mutate(match = basename(page_url)) %>% 
+  mutate(match_name = match) %>% 
+  separate(match, into = c("home", "away"), sep = "-vs-") %>% 
+  mutate(home = toupper(home)) %>% 
+  mutate(match = match_name)
+
+
+
+
+
+
+league_table <- fotmob_get_league_tables(league_id = 77)
+
+groups <- league_table %>% 
+  mutate(home = toupper(table_name)) %>% 
+  select(group_name, home) %>% 
+  distinct(home, .keep_all = TRUE)
+
+
+ 
+
+
+all_matches <- matches  %>% 
+  left_join(groups, by = "home")
